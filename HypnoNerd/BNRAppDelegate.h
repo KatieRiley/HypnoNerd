96624a0975670605149c94f58f8893b34e66a151
Katie Riley <katie.ann.riley@hotmail.com>
2014-03-20 09:36:44 -0700

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Katie Hoskins on 3/20/14.
+//  Copyright (c) 2014 Katie Hoskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
